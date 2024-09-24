411061ccb5355ac95c6eccf1f102b1585e54f87e
PabloCodzer <pablo.cozer@gmail.com>
2024-09-24 15:36:41 -0300

    adicionado endpoint para Adicionado endpoint para upload/download de imagens

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FAST-API-DDD-TDD
 Projeto de API RestFull, utilizando FastApi, Sql Alchemy, Postgres 
+
+Adicionado endpoint para upload/download de imagens. 
--- SE04/api/V1/api.py
+++ SE04/api/V1/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from api.V1.endpoists import curso
+from api.V1.endpoists import curso, curso_images
+
 
 api_router = APIRouter()
 api_router.include_router(curso.router, prefix='/cursos', tags=['cursos'])
+api_router.include_router(curso_images.router, prefix='/images', tags=['cursos'])
